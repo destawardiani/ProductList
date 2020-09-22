5fd6a8d5dca80280dbfba3f7d622310b31e56f77
destawardiani <destawardiani@gmail.com>
2020-09-22 19:53:39 +0700

    checkpoint 5

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+    container: {
+        backgroundColor: '#eeeeee',
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 0, height: 2},
+        elevation: 2
+    },
+    text: {
+        fontSize: 25
+    }
+})
 
 class Header extends Component {
     render() {
       return (
-        <View>
-            <Text>Header</Text>
+        <View style={ style.container }>
+          <Text style={ style.text }>
+              { this.props.title}
+          </Text>
         </View>
       )
     }
