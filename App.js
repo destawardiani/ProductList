3aceaf710ed970713869a3c8e8347066d58d4e37
destawardiani <destawardiani@gmail.com>
2020-09-23 10:55:30 +0700

    checkpoint 6

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 import Header from './src/components/Header'
+import ProductList from './src/components/ProductList'
 
 class App extends Component {
   render() {
     return (
       <View>
         <Header title="Product List" />
-        <Text>Hello World</Text>
+        <ProductList />
       </View>
     )
   }
 }
+
 export default App;
---
+++ src/components/ProductList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ProductList extends Component {
+    state = { }
+    render() {
+      return (
+        <View>
+          <Text>Product List</Text>
+        </View>
+      )
+    }
+  }
+  export default ProductList;
