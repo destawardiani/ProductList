57c102dacf34976469b5f85acd0814af2b9ef3f7
destawardiani <destawardiani@gmail.com>
2020-09-23 11:54:02 +0700

    checkpoint 7

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class ProductList extends Component {
-    state = { }
+    state = { 
+        products: []
+     }
+
+     componentDidMount() {
+        axios
+            .get('https://simple-ecommerce-9999.herokuapp.com/api/v1/product')
+            .then(({ data, status }) => {
+                console.log('CODE: ' + data.code);
+                console.log('DATA: ' + data.data);
+
+                this.setState({ products: data.data })
+            })
+     }
+
+     renderProducts() {
+        return this.state.products.map(product => 
+        <Text key={ product.id }>
+            { product.name }
+        </Text>
+        )
+    }
+
     render() {
       return (
         <View>
-          <Text>Product List</Text>
+          { this.renderProducts() }
         </View>
       )
     }
